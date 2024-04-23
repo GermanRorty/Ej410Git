b9711d558c9d52250d1f93065ea35e569d8197d4
GermanRM <germanr.marty@outlook.es>
2024-04-23 22:14:13 +0200

    primer commit Ej4.10 Git

---
+++ Ej4.10Git/src/ej4/pkg10git/Ej410Git.java
@@ -0,0 +1,12 @@
+package ej4.pkg10git;
+
+/**
+ *
+ * @author German
+ */
+public class Ej410Git {
+
+    public static void main(String[] args) {
+        System.out.println(Sumador.sumar(15f, 4f));
+    }
+}
